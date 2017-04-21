c2bbb2d28b84815baf600162d4c5c19dcfe392e0
Bruce Merry <bmerry@gmail.com>
2017-04-21 15:11:34 +0200

    Add example of using the listener API

---
+++ python/dl24/examples/listener_demo.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import asyncio
+import dl24.listener
+
+
+async def async_main():
+    conn = dl24.listener.open_file_connection('proxy.log')
+    while True:
+        async for record in dl24.listener.proxy_log_records(conn):
+            print('Next record')
+            print(record.message)
+
+
+if __name__ == '__main__':
+    asyncio.get_event_loop().run_until_complete(async_main())
