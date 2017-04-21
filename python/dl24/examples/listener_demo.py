f3e1380711cc8ce001b73ac968ee6f58e97fc990
Carl Hultquist <chultquist@gmail.com>
2017-04-21 15:47:32 +0100

    Fallback to a polling observer in listener demo

--- python/dl24/examples/listener_demo.py
+++ python/dl24/examples/listener_demo.py
@@ -2,9 +2,10 @@
 import asyncio
 import dl24.listener
 
+from watchdog.observers.polling import PollingObserver
 
 async def async_main():
-    conn = dl24.listener.open_file_connection('proxy.log')
+    conn = dl24.listener.open_file_connection('proxy.log', observer=PollingObserver())
     while True:
         async for record in dl24.listener.proxy_log_records(conn):
             print('Next record')
